8c12a76197c61f23cd12f3072b767f060ad10bc2
jasonmm24 <jason240208@gmail.com>
2026-04-08 20:36:56 -0600

    README

--- simulacion_ws/build/motor_msgs/motor_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jason/C-ROS-Automodel_Car/simulacion_ws/build/motor_msgs/motor_msgs__py
-# Build directory: /home/jason/C-ROS-Automodel_Car/simulacion_ws/build/motor_msgs/motor_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
