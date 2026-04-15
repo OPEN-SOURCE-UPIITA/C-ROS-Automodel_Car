8bccf5f055b690e1afef8d3d37edc68ce8d6f216
jonsdr <jon_sdr@protonmail.com>
2026-04-15 11:26:22 -0600

    Resolucion: auto_setup/launch/hardware... Launch de launch para el ascamera

--- AutoModelRasp_ws/src/auto_setup/launch/hardware.launch.py
+++ AutoModelRasp_ws/src/auto_setup/launch/hardware.launch.py
@@ -3,25 +3,22 @@
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
-from launch.substitutions import FindExecutable
-
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
 def generate_launch_description():
     return LaunchDescription([
         # 1. Cámara Astra HP60C
-        Node(
-            package='ascamera_hp60c',
-            executable='camera_publisher',
-            name='ascamera_hp60c',
-            output='screen',
-            parameters=[{
-                'device_id': 0,
-                'fps': 30,
-                'resolution': '640x480'
-            }]
-        ),
-        
-        # 2. Comunicación con STM32 (UART)
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('ascamera'),
+                    'launch',
+                    'hp60c.launch.py'
+                ])
+            ])
+        ),        # 2. Comunicación con STM32 (UART)
         Node(
             package='com_stm',
             executable='com_stm',
